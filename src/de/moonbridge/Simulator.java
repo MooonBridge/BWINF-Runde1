175c5b04c9aa0442fde051204ad279d27de527a6
BuildTools <unconfigured@null.spigotmc.org>
2021-11-16 20:22:08 +0100

    Simulator Update 2

--- src/de/moonbridge/Simulator.java
+++ src/de/moonbridge/Simulator.java
@@ -1,6 +1,7 @@
 package de.moonbridge;
 
 import java.util.Arrays;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Simulator {
 
@@ -14,26 +15,37 @@ public class Simulator {
     int winner = 0;
 
     public Simulator(int[] wuerfel1, int[] wuerfel2){
-
         w1seiten = wuerfel1[0];
         w2seiten = wuerfel2[0];
-
         w1 = Arrays.copyOfRange(wuerfel1, 1, wuerfel1.length);
         w2 = Arrays.copyOfRange(wuerfel2, 1, wuerfel2.length);
-
     }
 
     public int startGame(){
 
-        if(winner == 1){
-
-            return 1;
-
-        } else {
-
-            return 2;
-
+        if (wuerfeln(w1) != wuerfeln(w2)){
+            if (wuerfeln(w1) > wuerfeln(w2)){
+                gameActive(1);
+            } else {
+                gameActive(2);
+            }
         }
+
+        if(winner == 1){
+            return 1;
+        } else {
+            return 2;
+        }
+
+    }
+
+    public void gameActive(int firstPlayer){
+
+    }
+
+    public int wuerfeln(int[] w){
+
+        return w[ThreadLocalRandom.current().nextInt(0, w.length)];
 
     }
 
